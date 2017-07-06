3cb4bba707bdf06573c7bac5b2454258698c2312
Ka Tam <nkltam2003@gmail.com>
2017-07-06 16:19:54 -0700

    Fix "renders without crashing" test case
    add redux Provider setup to basic test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
+import { reducer as reduxFormReducer } from 'redux-form'
 import App from './App';
+import WizardForm from './forms/WizardForm'
+
+const reducer = combineReducers({
+  form: reduxFormReducer // mounted under "form"
+})
+const store = (window.devToolsExtension
+  ? window.devToolsExtension()(createStore)
+  : createStore)(reducer)
+
+const showResults = values =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      // simulate server latency
+      window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
+      resolve()
+    }, 500)
+  })
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+  div);
 });
